1df49f9dcc1c1c9d3ebaf04f4275ee8065dfa88f
Zakir Magdum <zakirmagdum2000@yahoo.com>
2015-12-29 20:57:32 -0500

    Integration with wro

---
+++ multitenant/src/main/java/org/zama/examples/multitenant/rest/HelloWorldResource.java
@@ -0,0 +1,25 @@
+package org.zama.examples.multitenant.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * HelloWorldResource.
+ *
+ * @author Zakir Magdum
+ */
+@RestController
+@RequestMapping("/")
+public class HelloWorldResource {
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+}
